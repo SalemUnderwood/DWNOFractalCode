a74ea42f0df8d31c2a89f1dc45bb8772eb7262bb
Ceeige <78670343+Ceeige@users.noreply.github.com>
2025-01-27 22:11:21 -0500

    Apply .gitignore file again

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace DWNOFractalCode
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "DWNOFractalCode";
-        public const string PLUGIN_NAME = "DWNOFractalCode";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
